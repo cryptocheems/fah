3c32335a590cf025a54cd018e798a1d16d00d738
kowasaur <kowasaur@gmail.com>
2021-07-12 20:50:52 +1000

    start sheets functions

--- distribute/drive.py
+++ distribute/drive.py
@@ -8,8 +8,9 @@ from google.oauth2.credentials import Credentials
 from googleapiclient.http import MediaFileUpload
 from constants import TOKEN_PATH, CRED_PATH
 
-SCOPES = ["https://www.googleapis.com/auth/drive"]
+SCOPES = ["https://www.googleapis.com/auth/drive", "https://www.googleapis.com/auth/spreadsheets"]
 FOLDER_ID = "1gPNzISk1AOfJAMiN1KwZ9RCdakwaIgHi"
+SHEET_ID = "1DvfcHKmsitkXS9sQ5fuTwesuIwlTq_fzNHQ30NdIlXs"
 
 
 class Drive:
@@ -32,6 +33,9 @@ class Drive:
                 token.write(creds.to_json())
 
         self.service = build("drive", "v3", credentials=creds)
+        self.sheets = build("sheets", "v4", credentials=creds).spreadsheets()
+
+    # region Drive
 
     def _upload_file_data(self, metadata, media: MediaFileUpload = None) -> str:
         data = self.service.files().create(body=metadata, fields="id", media_body=media).execute()
@@ -50,3 +54,70 @@ class Drive:
         media = MediaFileUpload(f"{path}/{filename}")
         file_metadata = {"name": filename, "parents": [folder_id]}
         return self._upload_file_data(file_metadata, media=media)
+
+    # endregion
+
+    # region Sheets
+
+    def _batch_requests(self, requests):
+        if type(requests) == dict:
+            requests = [requests]
+        r = {"requests": requests}
+        return self.sheets.batchUpdate(spreadsheetId=SHEET_ID, body=r).execute()
+
+    def create_sheet(self, title: str) -> int:
+        response = self._batch_requests({"addSheet": {"properties": {"title": title, "index": 1}}})
+        return response["replies"][0]["addSheet"]["properties"]["sheetId"]
+
+    def upload_to_sheet(self, csv_path: Path, sheet_id: int, rows: int):
+        with open(csv_path) as f:
+            csv = f.read()
+        requests = [
+            {  # CSV
+                "pasteData": {
+                    "coordinate": {"sheetId": sheet_id},
+                    "data": csv,
+                    "type": "PASTE_NORMAL",
+                    "delimiter": ",",
+                },
+            },
+            {  # Alternate colours
+                "addBanding": {
+                    "bandedRange": {
+                        "range": {"sheetId": sheet_id, "endColumnIndex": 4, "endRowIndex": rows},
+                        "rowProperties": {
+                            "headerColor": {  # orange/yellow
+                                "red": 247 / 255,
+                                "green": 203 / 255,
+                                "blue": 77 / 255,
+                            },
+                            "firstBandColor": {  # white
+                                "red": 1,
+                                "green": 1,
+                                "blue": 1,
+                            },
+                            "secondBandColor": {  # light yellow
+                                "red": 254 / 255,
+                                "green": 248 / 255,
+                                "blue": 227 / 255,
+                            },
+                        },
+                    }
+                },
+            },
+            {  # Change first column width
+                "updateDimensionProperties": {
+                    "range": {
+                        "sheetId": sheet_id,
+                        "dimension": "COLUMNS",
+                        "startIndex": 0,
+                        "endIndex": 1,
+                    },
+                    "properties": {"pixelSize": 340},
+                    "fields": "pixelSize",
+                }
+            },
+        ]
+        self._batch_requests(requests)
+
+    # endregion
